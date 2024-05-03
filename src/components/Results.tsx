93349fa02489a1a1183899dc9c12d30384e117a6
ismail-hepl <ismail.s@hepl.com>
2024-05-03 23:25:38 +0530

    Feat: Added the animation effect using framer motion

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,3 +1,6 @@
+import { motion } from "framer-motion";
+import { formatPercentage } from "../utils/helpers";
+
 const Results = ({
 	errors,
 	accuracyPercentage,
@@ -9,15 +12,48 @@ const Results = ({
 	total: number,
 	className: string
 }) => {
+
+	const initial = { opacity: 0 };
+	const animate = { opacity: 1 };
+	const duration = { duration: 0.3 };
+
 	return (
-		<ul
+		<motion.ul
 			className={`flex flex-col items-center text-primary-400 space-y-3 ${className}`}
 		>
-			<li className="text-xl font-semibold">Results</li>
-			<li className="font-bold">Accuracy: {accuracyPercentage}</li>
-			<li className="text-red-500 font-bold">Errors: {errors}</li>
-			<li className="font-bold">Typed: {total}</li>
-		</ul>
+			<motion.li
+				initial={initial}
+				animate={animate}
+				className="text-xl font-semibold"
+				transition={{ ...duration, delay: 0 }}
+			>
+				Results
+			</motion.li>
+			<motion.li
+				initial={initial}
+				animate={animate}
+				className="font-bold"
+				transition={{ ...duration, delay: 0.5 }}
+			>
+				Accuracy: {accuracyPercentage}
+			</motion.li>
+			<motion.li
+				initial={initial}
+				animate={animate}
+				className="text-red-500 font-bold"
+				transition={{ ...duration, delay: 1 }}
+			>
+				Errors: {errors}
+			</motion.li>
+			<motion.li 
+				initial={initial}
+				animate={animate}
+				className="font-bold"
+				transition={{ ...duration, delay: 1.5 }}
+			>
+				Typed: {total}
+			</motion.li>
+		</motion.ul>
 	)
 }
 
