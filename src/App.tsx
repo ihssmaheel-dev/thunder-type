3b300cc5c15e6e313190b2e0c4bbf9bb108932f9
ismail-hepl <ismail.s@hepl.com>
2024-05-03 22:38:46 +0530

    Feat: Created the GeneratedWords function and rendered it in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import { faker } from "@faker-js/faker"
 
 const words = faker.word.words(10);
 
-const App = () => {
-  return <div className='text-4xl text-center text-slate-500'>{words}</div>
+const GeneratedWords = ({ words }: { words: string }) => {
+  return <div className='text-4xl text-center text-slate-500'>{words}</div>;
 }
+
+
+const App = () => {
+  return <GeneratedWords words={words}/>
+}
+
+
 
 export default App
