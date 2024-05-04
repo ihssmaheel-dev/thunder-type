2bd592e3eb38dcfb45342ed36e6865f0668acce2
ismail-hepl <ismail.s@hepl.com>
2024-05-04 15:47:06 +0530

    Feat: Imported useCountdownTImer hook to manage the timer in the useEngine hook

--- src/hooks/useEngine.ts
+++ src/hooks/useEngine.ts
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import useWords from "./useWords";
+import useCountdownTimer from "./useCountdownTimer";
 
 export type State = "start" | "run" | "finish";
 
 const NUMBER_OF_WORDS = 12;
+const COUNTDOWN_SECONDS = 30;
 
 const useEngine = () => {
     const [state, setState] = useState<State>("start");
     const { words, updatedWords } = useWords(NUMBER_OF_WORDS);
+    const { timeLeft, startCountdown, resetCountdown } = useCountdownTimer(COUNTDOWN_SECONDS);
 
-    return { state, words };
+
+    return { state, words, timeLeft };
 };
 
 export default useEngine;
