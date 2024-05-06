78ab89e9d2ee7dc6c8c24a8736fc5c4ae11c2b09
ismail-hepl <ismail.s@hepl.com>
2024-05-06 22:59:18 +0530

    Feat: Created isKeyboardCodeAllowed functions to restrict some unwanted keys and implemented it

--- src/hooks/useTypings.ts
+++ src/hooks/useTypings.ts
@@ -1,11 +1,36 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { KeyboardEvent, useCallback, useEffect, useRef, useState } from "react"
+
+const isKeyboardCodeAllowed = (code: string) => {
+	return (
+		code.startsWith("key") || 
+		code.startsWith("Digit") ||
+		code === "Backspace" ||
+		code === "Space"
+	);
+}
 
 const useTypings = (enabled: boolean) => {
 	const [cursor, setCursor] = useState(0);
 	const [typed, setTyped] = useState<string>("");
 	const totalTyped = useRef(0);
 
-	const keydownHandler = useCallback(() => {}, []);
+	const keydownHandler = useCallback(({ key, code } : KeyboardEvent) => {
+		if(!enabled || !isKeyboardCodeAllowed(code)) {
+			return;
+		}
+
+		switch(key) {
+			case "Backspace":
+				setTyped((prev) => prev.slice(0, -1));
+				setCursor(cursor - 1);
+				totalTyped.current -= 1;
+				break;
+			default:
+				setTyped((prev) => prev.concat(key));
+				setCursor(cursor + 1);
+				totalTyped.current += 1;
+		}
+	}, [cursor, enabled]);
 
 	//attach the keydown event listener to record keystrokes
 	useEffect(() => {
