380065d44a5d8657cb6258c675231209e6c35d9c
ismail-hepl <ismail.s@hepl.com>
2024-05-07 22:56:43 +0530

    Fix: rendering the expected instead of actual user typing in the typist screen

--- src/components/UserTypings.tsx
+++ src/components/UserTypings.tsx
@@ -1,14 +1,16 @@
 import Caret from "./Caret";
 
-const Character = ({ char }: { char: string }) => {
-	return <span className="text-primary-400">{char}</span>
+const Character = ({ actual, expected }: { actual: string, expected: string }) => {
+	return <span className="text-primary-400">{expected}</span>
 }
 
 const UserTypings = ({
 	userInput,
 	className,
+	words
 }:{
 	userInput: string,
+	words: string,
 	className?: string
 }) => {
 	const typedCharacters = userInput.split("");
@@ -16,7 +18,7 @@ const UserTypings = ({
 	return (
 		<div className={className}>
 			{typedCharacters.map((char, index) => {
-				return <Character key={`${char}_${index}`} char={char}/>
+				return <Character key={`${char}_${index}`} actual={char} expected={words[index]}/>
 			})}
 			<Caret />
 		</div>
