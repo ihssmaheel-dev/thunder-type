532c4a9c5b86aed92c1f94cf1ccf8b833376486f
ismail-hepl <ismail.s@hepl.com>
2024-05-10 23:06:07 +0530

    Feat: Added the new helper function called countErrors

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,16 @@
 export const formatPercentage = (percentage: number) => {
     return percentage.toFixed(0) + "%";
 }
+
+export const countErrors = (actual: string, expected: string) => {
+    const expectedCharacters = expected.split("");
+
+    return expectedCharacters.reduce((errors, expectedChar, i) => {
+        const actualChar = actual[i];
+        if(actualChar !== expectedChar) {
+            errors++;
+        }
+
+        return errors
+    }, 0);
+};
