8560e0a446bbed0e17ae41c820fbb993963b8177
ismail-hepl <ismail.s@hepl.com>
2024-05-12 15:55:49 +0530

    Feat: Added the new function called calculateWpm

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,6 +2,17 @@ export const formatPercentage = (percentage: number) => {
     return percentage.toFixed(0) + "%";
 }
 
+export const calculateWpm = (totalTyped: number, timeLeft: number, countdownSeconds: number) => {
+    if (totalTyped > 0) {
+        const elapsedTime = countdownSeconds - timeLeft;
+        const typedWords = totalTyped / 5; // Assuming 5 characters per word
+        const wordsPerMinute = (typedWords / (elapsedTime / 60)).toFixed(2);
+        return parseFloat(wordsPerMinute);
+    }
+
+    return 0;
+};
+
 export const countErrors = (actual: string, expected: string) => {
     const expectedCharacters = expected.split("");
 
